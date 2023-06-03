442e72c28a14dbeb0bfde4e0a19b8708ec5c2b71
ProtonNumber <jwhinton@yenno.com>
2023-06-03 06:29:30 +0100

    Formatting fix

--- libgen.sh
+++ libgen.sh
@@ -13,10 +13,10 @@ PICO_SDK_PATH=./pico-sdk
 PATHS=$(make lib -n -f RP2040.Makefile | sed 's/ /\n/g' | grep -F ./ | sort | uniq)
 
 # Get all include paths. Add commas to the end (PIO expects a list.)
-INC=$(echo "$PATHS" | grep -F 'I.' | sed '$!s/$/,/')
+INC=$(echo "$PATHS" | grep -F 'I.' | sed 's/.*/"&",/')
 
 SRC=$(echo "$PATHS" | grep '\.[cS]$' | sed 's/.*/+<&>/')
 
 BLANK_JSON=${BLANK_JSON}
 
-echo { \"name\": \"ChibiOS\", \"version\": \"21\", \"build\": { \"includeDir\": \".\", \"flags\": [ -Iinclude, -Wl,--defsym=__process_stack_size__=0x400, -Wl,--defsym=__main_stack_size__=0x200, -DCRT0_EXTRA_CORES_NUMBER=1, $INC ] \"srcDir\": \".\", \"srcFilter\" : $SRC, } } |  sed 's/ /\n/g' > library.json
+echo { \"name\": \"ChibiOS\", \"version\": \"21\", \"build\": { \"includeDir\": \".\", \"flags\": [ -Iinclude, -Wl,--defsym=__process_stack_size__=0x400, -Wl,--defsym=__main_stack_size__=0x200, -DCRT0_EXTRA_CORES_NUMBER=1, $INC ] \"srcDir\": \".\", \"srcFilter\" : \"$SRC\", } } |  sed 's/ /\n/g' > library.json
